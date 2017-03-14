09f88df258fccbf224a51aa139203cec168e9c32
stefan0303 <tepsiev@hotmail.com>
2017-03-14 15:47:39 +0200

    Stefan - 14/03/2017

---
+++ MVC-Blog/Models/Post.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCBlog.Models
+{
+    public class Post
+    {
+        public Post()
+        {
+            this.Date = DateTime.Now;
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        public ApplicationUser Author { get; set; }
+    }
+}
