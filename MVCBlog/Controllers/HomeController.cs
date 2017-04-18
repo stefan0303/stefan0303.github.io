15590ef951f325028b3e5cc27f8c4ad9a7415fdb
stefan0303 <tepsiev@hotmail.com>
2017-04-18 11:46:45 +0300

    folder name

---
+++ MVCBlog/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCBlog.Models;
+using System.Data.Entity;
+
+namespace MVCBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        //Get:Posts
+        public ActionResult Index()
+        {
+            
+            var posts = db.Posts.Include(p => p.Author).OrderByDescending(p => p.Date).Take(5);
+            return View(posts.ToList());
+        }
+
+    }
+}
---
+++ MVCBlog/HelpClass/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCBlog.HelpClass
+{
+    public class Utils
+    {
+        public static string MakeCutText(string text,int maxLenght)
+        {
+            if (text==null)
+            {
+                return null;
+            }
+            else if (text.Length>maxLenght)
+            {
+                return text.Substring(0, maxLenght)+ ".....";
+            }
+            else
+            {
+                return text;
+            }
+          
+        }
+    }
+}
